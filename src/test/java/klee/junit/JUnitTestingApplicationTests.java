c0d8884d8097facd158833fce7fba8adf0a8e503
Klee Collage <klee.collage@gmail.com>
2025-04-08 16:41:12 -0600

    First commit

---
+++ src/test/java/klee/junit/JUnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package klee.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
