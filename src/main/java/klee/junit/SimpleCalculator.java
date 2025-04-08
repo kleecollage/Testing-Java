898749662ae59d26bad237224f21cdb8c7e86e9f
Klee Collage <klee.collage@gmail.com>
2025-04-08 16:54:51 -0600

    SimpleCalculatorTest

---
+++ src/main/java/klee/junit/SimpleCalculator.java
@@ -0,0 +1,7 @@
+package klee.junit;
+
+public class SimpleCalculator {
+    public int add (int numberA, int numberB) {
+        return numberA + numberB;
+    }
+}
---
+++ src/test/java/klee/junit/SimpleCalculatorTest.java
@@ -0,0 +1,19 @@
+package klee.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleCalculatorTest {
+    @Test
+    void twoPlusTwoShouldEqualFour() {
+        var calculator = new SimpleCalculator();
+        assertEquals(4, calculator.add(2, 2));
+    }
+
+    @Test
+    void threePlusSevenShouldEqualTen() {
+        var calculator = new SimpleCalculator();
+        assertEquals(10, calculator.add(3, 7));
+    }
+}
